4541505e22df6fc1fec48b5d0f7df7a0b041c244
Hashitha <mahniroshan16@gmail.com>
2023-01-23 12:06:46 +0530

    modified evaluations_details

--- Flutter/Feedback_Frontend/campus-feedback-frontend/lib/src/screens/evaluation_details.dart
+++ Flutter/Feedback_Frontend/campus-feedback-frontend/lib/src/screens/evaluation_details.dart
@@ -38,10 +38,10 @@ class EvaluationDetailsScreen extends StatelessWidget {
               evaluation!.evaluation_criteria_id!.toString(),
               style: Theme.of(context).textTheme.headline4,
             ),
-            // Text(
-            //   evaluation!.activity_instance_id!.toString(),
-            //   style: Theme.of(context).textTheme.headline4,
-            // ),
+            Text(
+              evaluation!.activity_instance_id!.toString(),
+              style: Theme.of(context).textTheme.headline4,
+            ),
             Text(
               evaluation!.grade!.toString(),
               style: Theme.of(context).textTheme.headline4,
